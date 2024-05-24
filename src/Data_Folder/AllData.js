1a906ad03f1e4658e542a1e346daedb1313c526a
HassanWebdev <HassanWebdev@github.com>
2024-05-24 12:13:54 +0500

    first commit

---
+++ src/Data_Folder/AllData.js
@@ -0,0 +1,242 @@
+import img0 from '../assets/products/cardio/american.jpg';
+import img1 from '../assets/products/cardio/proForm.jpg';
+import img2 from '../assets/products/cardio/rfc.jpeg';
+import img3 from '../assets/products/cardio/rocket.jpg';
+import img4 from '../assets/products/cardio/rower.png';
+import img5 from '../assets/products/cardio/treadmil.jpg';
+import img6 from '../assets/products/weight/adjustableBench.jpg';
+import img7 from '../assets/products/weight/benchPrechure.jpeg';
+import img8 from '../assets/products/weight/flatBench.jpg';
+import img9 from '../assets/products/weight/proFormSport.jpg';
+import img10 from '../assets/products/weight/rod.jpg';
+import img11 from '../assets/products/weight/slimLine.jpg';
+import img12 from '../assets/products/commercial/abdominal.jpg';
+import img13 from '../assets/products/commercial/cableCross.jpg';
+import img14 from '../assets/products/commercial/calf.jpg';
+import img15 from '../assets/products/commercial/chest.jpg';
+import img16 from '../assets/products/commercial/dipChin.jpg';
+import img17 from '../assets/products/commercial/lat.jpg';
+import img18 from '../assets/products/commercial/seated.jpg';
+import img19 from '../assets/products/commercial/seated.jpg';
+
+const data = [
+  {
+    "id": 0,
+    "img": img0,
+    "title": "AMERICAN FITNESS TREADMILL 141-E",
+    "quantity": 10,
+    "price": 64100
+  },
+  {
+    "id": 1,
+    "img": img1,
+    "title": "Pro-form Treadmill Pro-2000E",
+    "quantity": 10,
+    "price": 24100
+  },
+  {
+    "id": 2,
+    "img": img2,
+    "title": "Pro-form USA Spinner Bike 500-SPX",
+    "quantity": 110,
+    "price": 4100
+  },
+  {
+    "id": 3,
+    "img": img3,
+    "title": "Mini Bike Pedal Exerciser, Without Meter for Legs Arms Physical Therapy Workout",
+    "quantity": 8,
+    "price": 6400
+  },
+  {
+    "id": 4,
+    "img": img1,
+    "title": "Pro-form Treadmill Pro-2000",
+    "quantity": 10,
+    "price": 10000
+  },
+  {
+    "id": 5,
+    "img": img0,
+    "title": "American Fitness Recumbent Bike Model BR5901-R",
+    "quantity": 7,
+    "price": 14000
+  },
+  {
+    "id": 6,
+    "img": img2,
+    "title": "DKALN Taiwan Brand Bicycle 26 Inch Aluminum Alloy Mountain Bike Disc Brake 880",
+    "quantity": 8,
+    "price": 61400
+  },
+  {
+    "id": 7,
+    "img": img3,
+    "title": "Mini Bike Pedal Exerciser, Without Meter for Legs Arms Physical Therapy Workout",
+    "quantity": 8,
+    "price": 6400
+  },
+  {
+    "id": 8,
+    "img": img5,
+    "title": "WB-7080 Multi Home Gym Machine All In One for Multiple Workout",
+    "quantity": 5,
+    "price": 35000
+  },
+  {
+    "id": 9,
+    "img": img0,
+    "title": "American Fitness Recumbent Bike Model BR5901-R",
+    "quantity": 10,
+    "price": 350000
+  },
+  {
+    "id": 10,
+    "img": img4,
+    "title": "American Fitness Rowing Machine Model HC101-A",
+    "quantity": 18,
+    "price": 64400
+  },
+  {
+    "id": 11,
+    "img": img6,
+    "title": "FDC Carbon Strength Home Gym",
+    "quantity": 10,
+    "price": 14546
+  },
+  {
+    "id": 12,
+    "img": img7,
+    "title": "Multi-function Strength Trainer Bench Model 6003-G With Preacher",
+    "quantity": 2,
+    "price": 14505
+  },
+  {
+    "id": 13,
+    "img": img8,
+    "title": "WB-7080 Multi Home Gym Machine All In One for Multiple Workout",
+    "quantity": 10,
+    "price": 14546
+  },
+  {
+    "id": 14,
+    "img": img9,
+    "title": "Home Gym Machine Model 1208T",
+    "quantity": 7,
+    "price": 145546
+  },
+  {
+    "id": 15,
+    "img": img10,
+    "title": "Pro-form Treadmill Pro-2000",
+    "quantity": 10,
+    "price": 10000
+  },
+  {
+    "id": 16,
+    "img": img11,
+    "title": "American Fitness Recumbent Bike Model BR5901-R",
+    "quantity": 7,
+    "price": 14000
+  },
+  {
+    "id": 17,
+    "img": img6,
+    "title": "DKALN Taiwan Brand Bicycle 26 Inch Aluminum Alloy Mountain Bike Disc Brake 880",
+    "quantity": 8,
+    "price": 61400
+  },
+  {
+    "id": 18,
+    "img": img7,
+    "title": "Mini Bike Pedal Exerciser, Without Meter for Legs Arms Physical Therapy Workout",
+    "quantity": 8,
+    "price": 6400
+  },
+  {
+    "id": 19,
+    "img": img8,
+    "title": "WB-7080 Multi Home Gym Machine All In One for Multiple Workout",
+    "quantity": 5,
+    "price": 35000
+  },
+  {
+    "id": 20,
+    "img": img9,
+    "title": "American Fitness Recumbent Bike Model BR5901-R",
+    "quantity": 10,
+    "price": 350000
+  },
+  {
+    "id": 21,
+    "img": img10,
+    "title": "American Fitness Rowing Machine Model HC101-A",
+    "quantity": 18,
+    "price": 64400
+  },
+  {
+    "id": 22,
+    "img": img12,
+    "title": "Abdominal",
+    "quantity": 8,
+    "price": 600
+  },
+  {
+    "id": 23,
+    "img": img13,
+    "title": "Assisted Chin/Dip",
+    "quantity": 81,
+    "price": 800
+  },
+  {
+    "id": 24,
+    "img": img14,
+    "title": "Back Extension",
+    "quantity": 10,
+    "price": 80000
+  },
+  {
+    "id": 25,
+    "img": img15,
+    "title": "BTM-001 Chest Press",
+    "quantity": 110,
+    "price": 50000
+  },
+  {
+    "id": 26,
+    "img": img16,
+    "title": "Mini Bike Pedal Exerciser, Without Meter for Legs Arms Physical Therapy Workout",
+    "quantity": 8,
+    "price": 6400
+  },
+  {
+    "id": 27,
+    "img": img17,
+    "title": "ROYAL FITNESS CANADA TREADMILL T510-C",
+    "quantity": 10,
+    "price": 160000
+  },
+  {
+    "id": 28,
+    "img": img18,
+    "title": "Slimline Multi-function Strength Trainer Bench Model 7302",
+    "quantity": 7,
+    "price": 61400
+  },
+  {
+    "id": 29,
+    "img": img19,
+    "title": "Slimline Treadmill TA-4000",
+    "quantity": 5,
+    "price": 64100
+  },
+  {
+    "id": 30,
+    "img": img12,
+    "title": "WB-7080 Multi Home Gym Machine All In One for Multiple Workout",
+    "quantity": 5,
+    "price": 35000
+  }
+];
+
+export default data;
